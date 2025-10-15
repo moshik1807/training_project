77b5cfcdb84c1f2a9912f4b53c767f9c3dda2e90
moshik <moshik0527669169@gmail.com>
2025-10-15 12:30:03 +0300

    Deleting unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,6 @@ import LoginPage from "./pages/loginPage";
 import TrainersPage from "./pages/trainersPage";
 import TrainerPage from "./pages/trainerPage";
 import TrainingsPage from "./pages/trainings";
-// import { configureStore } from "@reduxjs/toolkit";
-// import { Provider } from "react-redux";
-// import counterSlice from "./rty"
-// import Counter from "./rty";
-// const mystor =configureStore({
-//   reducer:{
-//     counterSlice
-//   }
-// })
-
 
 export default function App() {
   return (
