ae652d8fd4a293978a6c5f02e8591e3b94d77431
moshik <moshik0527669169@gmail.com>
2025-10-29 12:32:49 +0200

    added training card

---
+++ src/components/trainingCard.jsx
@@ -0,0 +1,24 @@
+import { useSelector,useDispatch } from "react-redux"
+import { BUTTON } from "./button"
+import { deleteTraining } from "../features/trainings/trainingsThunk"
+
+export function Training({training}){
+    const trainers = useSelector(state => state.trainers.trainers)
+    const trainer = trainers.filter((e)=> e.id == training.coachId)
+    const dispatch = useDispatch()
+    const user = useSelector(state => state.user.user)
+
+
+    const handleDelete = ()=>{
+        dispatch(deleteTraining(training.id))
+    }
+
+    return(
+        <section style={{color:"white",width:"200px",height:"200px"}}>
+            <h1>{trainer[0].name}</h1>
+            <h2>{user.name}</h2>
+            <h3>{training.date}  {training.time}</h3>
+            <BUTTON onClick={handleDelete}>🗑️</BUTTON>
+        </section>
+    )
+}
--- src/pages/trainings.jsx
+++ src/pages/trainings.jsx
@@ -1,10 +1,18 @@
 import Navbar from "../components/navbar"
+import { Training } from "../components/trainingCard"
+import { useSelector } from "react-redux"
 
 export default function TrainingsPage() {
+    const trainings = useSelector(state => state.trainings.trainings)
     return(
         <>
         <Navbar/>
         <h1>trainings</h1>
+        {trainings.map((training,i)=>(
+            <div key={i}>
+                <Training training={training}/>
+            </div>
+        ))}
         </>
     )
 }
